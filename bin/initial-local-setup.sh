8ab129df32a66a3a19c65c4ff53fbc8173b1054f
John McKerrell <john@mckerrell.net>
2014-07-04 12:52:31 +0100

    Initial local setup, moved into single script.

---
+++ bin/initial-local-setup.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+usage() {
+    echo "usage: initial-local-setup.sh <path-to-sd>"
+}
+
+if [ "$1" = "" ]; then
+    usage
+    exit
+fi
+
+SDPATH="$1"
+
+touch "$SDPATH/etc/SSHFLAG"
+
+cp scripts/enable-ssh.sh "$SDPATH/sbin/"
+
+perl -p -i -e 's/exit 0/sh \/sbin\/enable-ssh.sh\nexit 0/' "$SDPATH/etc/rc.local"
+
+echo "Please enter SSID or leave blank for DoESLiverpool."
+read USER_SSID
+if [ "$USER_SSID" == "" ]; then
+    SSID="DoESLiverpool"
+else
+    SSID="$USER_SSID"
+fi
+echo "Please enter password for WiFi - $SSID"
+read PASSWORD
+
+sed 's/wpa-roam/#wpa-roam/' "$SDPATH/etc/network/interfaces"
+
+cat - > "$SDPATH/etc/wpa.conf" <<EOF
+network={
+ssid="$SSID"
+proto=RSN
+key_mgmt=WPA-PSK
+pairwise=CCMP TKIP
+group=CCMP TKIP
+psk="$PASSWORD"
+}
+EOF
+
+rm "$SDPATH/etc/profile.d/raspi-config.sh"
