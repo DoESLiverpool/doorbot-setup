0d1693cf0e661ed2025f5568f11de6cac096569e
John McKerrell <john@mckerrell.net>
2014-07-04 12:31:28 +0100

    Actually edit the rc.local, fix string comparison

--- bin/setup-ssh-locally.sh
+++ bin/setup-ssh-locally.sh
@@ -4,7 +4,7 @@ usage() {
     echo "usage: setup-ssh-locally.sh <path-to-sd>"
 }
 
-if [ "$1" == "" ]; then
+if [ "$1" = "" ]; then
     usage
     exit
 fi
@@ -15,4 +15,4 @@ touch "$SDPATH/etc/SSHFLAG"
 
 cp scripts/enable-ssh.sh "$SDPATH/sbin/"
 
-perl -p -e 's/exit 0/sh \/sbin\/enable-ssh.sh\nexit 0/' "$SDPATH/etc/rc.local"
+perl -p -i -e 's/exit 0/sh \/sbin\/enable-ssh.sh\nexit 0/' "$SDPATH/etc/rc.local"
